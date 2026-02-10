cf24ec9ecec3b00daa042d72e63a7ffe61467790
Yashas-webdev <yashasaryashu@gmail.com>
2026-02-10 21:49:30 +0530

    Add components folder with Header and StatsGrid

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <header>
+            <h1>🎯 Job Tracker</h1>
+            <p classname='subtitle'>Manage your job applications with ease</p>
+        </header>
+    );
+}
+
+export default Header;

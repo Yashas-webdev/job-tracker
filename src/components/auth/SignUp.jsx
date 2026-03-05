77e0f95b8ca94db09299e1b58884add5fb2bf4d7
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-05 22:20:30 +0530

    updated the SingUp.jsx and Auth.css

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -28,5 +28,128 @@ export default function SignUp({ onSwitchToSignIn }){
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
+    
+
+    if (formData.password !== formData.confirmPassword){
+        setError('Password do not match');
+        return;
     }
-}
+
+    if (formData.password.length < 6){
+        setError('Password must be at least 6 characters');
+        return;
+    }
+
+    setLoading(true);
+    try{
+        await signUp({
+            name:formData.name,
+            email:formData.email,
+            password:formData.password,
+            phone: formData.phone,
+            location: formData.location
+        });
+    }catch (err){
+        setError(err.message);
+    }finally{
+        setLoading(false);
+    }
+    };
+  return(<div className="auth-container">
+    <div className="auth-card">
+        <div className="auth-header">
+            <h2>Create Account</h2>
+            <p className="auth-subtitle"></p>
+        </div>
+
+        {error && (
+            <div className="error-message">
+                ⚠️ {error}
+                </div>
+        )}
+
+        <form onSubmit={handleSubmit} className="auth-form">
+            <div className="form-group">
+                <label>Full name *</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  placeholder="ex..Yashas"
+                  disabled={loading}
+                  />
+            </div>
+
+            <div className="form-group">
+                <label>Phone</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  placeholder="+91-8453453454"
+                  disabled={loading}
+                  />
+            </div>
+
+            <div className="form-group">
+                <label>Location</label>
+                <input
+                  type="text"
+                  name="location"
+                  value={formData.location}
+                  onChange={handleChange}
+                  placeholder="Banglore, India"
+                  disabled={loading}/>
+            </div>
+
+            <div className="form-group">
+            <label>Password *</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              minLength={6}
+              placeholder="At least 6 characters"
+              disabled={loading}
+            />
+          </div>
+
+            <div className="form-group">
+                <label>Confirm Password *</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                  placeholder="Re-enter passwrord"
+                  disabled={loading}/>
+            </div>
+
+            <button 
+            type="submit"
+            className="btn-primary auth-button"
+            disabled={loading}
+            >
+                {loading ? 'Creating Account...' : 'Sign Up'}
+            </button>
+        </form>
+
+        <p className="auth-switch">
+            Already have an account ? {''}
+            <button onClick={onSwitchToSignIn}
+            className="link-button"
+            disabled={loading}
+            >
+                Sign In
+            </button>
+        </p>
+    </div>
+  </div>
+  );
+};
